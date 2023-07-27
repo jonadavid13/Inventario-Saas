3243d09c5c4ccb945600ec842c0e6088a7385c06
Jonathan Nunez <jonadavid13@gmail.com>
2023-07-27 13:21:28 -0400

    update

--- frontend-inventario/src/App.js
+++ frontend-inventario/src/App.js
@@ -1,6 +1,27 @@
 import React from "react";
 import { HeaderMain } from "./components/HeaderMain";
 import "./assets/styles/index.scss";
+import $ from "jquery";
+
+const getData = () => {
+  $.ajax({
+    type: "POST",
+    url: "https://petrifying-grant.000webhostapp.com/select_pozos.php",
+    dataType: "json",
+    beforeSend: function () {
+      console.log("Esperando los datos del BACK");
+    },
+    success: function (resp) {
+      console.log(resp);
+    },
+    fail: function (jqXHR, textStatus, errorThown) {
+      console.log(textStatus, errorThown);
+    },
+    error: function (jqXHR, textStatus, errorThown) {
+      console.log(textStatus, errorThown);
+    },
+  });
+}
 
 function App() {
   return (
@@ -8,12 +29,7 @@ function App() {
       <div className="App">
         <HeaderMain/>
         <div className="d-flex">
-          <button name="action1" id="btn1" className="btn btn-primary">Pedir datos</button>
-          <button name="action1" id="btn1" className="btn btn-secondary">Pedir datos</button>
-          <button name="action1" id="btn1" className="btn btn-success">Pedir datos</button>
-          <button name="action1" id="btn1" className="btn btn-warning">Pedir datos</button>
-          <button name="action1" id="btn1" className="btn btn-danger">Pedir datos</button>
-          <button name="action1" id="btn1" className="btn btn-dark">Pedir datos</button>
+          <button name="boton1" id="btn1" className="btn btn-primary" onClick={getData()}>Test</button>
         </div>
       </div>
     </>
